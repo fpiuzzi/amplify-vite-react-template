f37845e060724116fdedf04d69d69dce8e63e64c
Tori <147161762+fpiuzzi@users.noreply.github.com>
2025-04-08 21:33:36 +0200

    correction auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,35 @@
+// amplify/backend.ts
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineAuth } from '@aws-amplify/backend';
 
-defineBackend({
-  auth,
-  data,
+export const backend = defineBackend({
+
+  auth: defineAuth({
+    loginWith: {
+      email: true,
+
+    }
+  }),
+});
+
+
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  Todo: a
+      .model({
+        content: a.string(),
+      })
+      .authorization((allow) => [
+        allow.owner()
+      ]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "userPool",
+  },
 });
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-import { Authenticator } from '@aws-amplify/ui-react';
 
-Amplify.configure(outputs);
+import { Authenticator } from '@aws-amplify/ui-react';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <Authenticator>
+        <Authenticator
+            components={{
+                SignIn: {
+                    Header() {
+                        return <h3>Connexion à votre compte</h3>;
+                    },
+                },
+                SignUp: {
+                    Header() {
+                        return <h3>Créer un nouveau compte</h3>;
+                    },
+                }
+            }}
+        >
             <App />
         </Authenticator>
     </React.StrictMode>
