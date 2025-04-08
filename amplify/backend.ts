6552bdad60762b5640bea57d7286b44e37c09941
Tori <147161762+fpiuzzi@users.noreply.github.com>
2025-04-08 22:08:10 +0200

    modif auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,35 +1,13 @@
 // amplify/backend.ts
 import { defineBackend } from '@aws-amplify/backend';
 import { defineAuth } from '@aws-amplify/backend';
+import { data } from './data/resource';
 
 export const backend = defineBackend({
-
-  auth: defineAuth({
-    loginWith: {
-      email: true,
-
-    }
-  }),
-});
-
-
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-
-const schema = a.schema({
-  Todo: a
-      .model({
-        content: a.string(),
-      })
-      .authorization((allow) => [
-        allow.owner()
-      ]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "userPool",
-  },
+    auth: defineAuth({
+        loginWith: {
+            email: true
+        }
+    }),
+    data
 });
